eaca962780f94d83037c8fa011c0d5522e2eda78
Denis Sukhoverkhov <at10tion.angry@yandex.ru>
2017-12-16 13:49:42 +0300

    факториал

---
+++ factorial.py
@@ -0,0 +1,21 @@
+# Факториал
+
+# рекурсивно
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+
+# итеравтивно
+def fac(n):
+    res = 1
+    for n in range(1, n + 1):
+        res *= n
+
+    return res
+
+
+print(factorial(5))
+print(fac(5))
