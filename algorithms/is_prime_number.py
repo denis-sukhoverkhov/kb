ea4f7bf069c841e051b0c295127f4444264f7bb7
Denis Sukhoverkhov <at10tion.angry@yandex.ru>
2017-12-18 12:30:57 +0300

    алгоритм поиска простого числа с учетом только нечетных чисел и со сложностью - корень квадратный из n

---
+++ algorithms/is_prime_number.py
@@ -0,0 +1,15 @@
+def is_prime(n):
+    """
+    Поиск простого числа
+    :param n:
+    :return:
+    """
+    if n % 2 == 0:
+        return n == 2
+    d = 3
+    while d * d <= n and n % d != 0:
+        d += 2
+    return d * d > n
+
+
+print(is_prime(27))
