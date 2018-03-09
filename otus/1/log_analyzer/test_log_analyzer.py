9d721ae2f75579a49d7ec39c351aed5859037150
Denis Sukhoverkhov <at10tion.angry@yandex.ru>
2018-03-09 20:36:21 +0300

    test skeleton

---
+++ otus/1/log_analyzer/test_log_analyzer.py
@@ -0,0 +1,17 @@
+import unittest
+
+
+class TestLogAnalyzer(unittest.TestCase):
+
+    def test_take_only_last_log(self):
+        pass
+
+    def test_processing_not_exist_log(self):
+        pass
+
+    def test_repeat_processing_the_same_log(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
