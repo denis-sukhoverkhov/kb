ece478bfd8e796bbaa7401f2815f81b5ce8ab83c
Denis Sukhoverkhov <at10tion.angry@yandex.ru>
2018-05-27 16:29:17 +0300

    python decorator example

---
+++ algorithms/decorator.py
@@ -0,0 +1,28 @@
+from functools import wraps
+from inspect import signature
+
+
+def good_decorator(f):
+    @wraps(f)
+    def wrapper(*args, **kwds):
+        print('Calling decorated function')
+        return f(*args, **kwds)
+
+    return wrapper
+
+
+def bad_decorator(func):
+    def wrapper(*args, **kwargs):
+        return func(*args, **kwargs)
+    return wrapper
+
+
+@good_decorator
+def example(a: int, b: list):
+    """Docstring"""
+    print('Called example function')
+
+
+print(signature(good_decorator(example)))
+print(signature(bad_decorator(example)))
+
