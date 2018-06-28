bf5201f4dfd9d6b2146d0e78d026895e7bcbd9f8
Denis Sukhoverkhov <at10tion.angry@yandex.ru>
2018-06-28 13:11:52 +0300

    example using socket

---
+++ sandbox/socket/client.py
@@ -0,0 +1,8 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(('127.0.0.1', 8888))
+s.sendall(b'Hello, world')
+data = s.recv(1024)
+s.close()
+print(f'полученные данные: {data}')
