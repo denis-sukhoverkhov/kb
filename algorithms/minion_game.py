3ab3e1b8540226d66da80b52264ad7a4503f3ce3
Denis <denis.sukhoverkhov@yandex.ru>
2019-03-28 21:04:08 +0300

    minion game

---
+++ algorithms/minion_game.py
@@ -0,0 +1,20 @@
+def minion_game(string):
+    vowels = ['A', 'E', 'I', 'O', 'U']
+    kevsc, stusc = 0, 0
+    for i in range(len(string)):
+        if s[i] in vowels:
+            kevsc += (len(s) - i)
+        else:
+            stusc += (len(s) - i)
+
+    if kevsc > stusc:
+        print(f"Kevin {kevsc}")
+    elif kevsc < stusc:
+        print(f"Stuart {stusc}")
+    else:
+        print("Draw")
+
+
+if __name__ == '__main__':
+    s = input()
+    minion_game(s)
