cf0419645823ddc731575428b760991e182d57bf
Denis <denis.sukhoverkhov@yandex.ru>
2019-09-21 20:31:08 +0300

    golang reverse string algorithm

--- golang/algorithms/revers_string.go
+++ golang/algorithms/revers_string.go
@@ -11,3 +11,24 @@ func ReverseString(s string) string {
 
 	return newString
 }
+
+func ReverseString2(s string) string {
+	i := 0
+	j := utf8.RuneCount([]byte(s)) - 1
+
+	var newString = s
+
+	for i <= j {
+		i_ := string(newString[i])
+		j_ := string(newString[j])
+
+		sl1 := newString[:i]
+		sl2 := newString[i+1 : j]
+		sl4 := newString[j+1:]
+		newString = sl1 + j_ + sl2 + i_ + sl4
+		i += 1
+		j -= 1
+	}
+
+	return newString
+}
