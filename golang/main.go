d830f800225042f7d7da957861ef6b51160e7553
Denis <denis.sukhoverkhov@yandex.ru>
2019-09-21 20:43:30 +0300

    run all

--- golang/main.go
+++ golang/main.go
@@ -6,20 +6,18 @@ import (
 )
 
 func main() {
-	//fmt.Println(algorithms.FactorialRec(10))
-	//fmt.Println(algorithms.FactorialIter(10))
+	fmt.Println(algorithms.FactorialRec(10))
+	fmt.Println(algorithms.FactorialIter(10))
 
-	//fmt.Println(algorithms.FastPowerRec(2, 10))
-	//fmt.Println(algorithms.FastPowerIter(2, 10))
+	fmt.Println(algorithms.FastPowerRec(2, 10))
+	fmt.Println(algorithms.FastPowerIter(2, 10))
 
-	//fmt.Println(algorithms.IsPrimeNumber(29))
+	fmt.Println(algorithms.IsPrimeNumber(29))
 
-	//fmt.Println(algorithms.FibRec(7))
-	//str := "qwerty"
-	//utf8.RuneCount([]byte(str))
+	fmt.Println(algorithms.FibRec(7))
 
 	fmt.Println(algorithms.FibIter(12))
 
 	fmt.Println(algorithms.ReverseString("i am programmist"))
-	//fmt.Println(algorithms.ReverseString2("i am programmist"))
+	fmt.Println(algorithms.ReverseString2("i am programmist"))
 }
