118a936cc106c4bdc783c2f57a99c7fbec39344d
Denis <denis.sukhoverkhov@yandex.ru>
2019-09-22 14:22:28 +0300

    golang panic example

--- golang/main.go
+++ golang/main.go
@@ -2,27 +2,26 @@ package main
 
 import (
 	"./algorithms"
-	"fmt"
 )
 
 func main() {
-	fmt.Println(algorithms.FactorialRec(10))
-	fmt.Println(algorithms.FactorialIter(10))
-
-	fmt.Println(algorithms.FastPowerRec(2, 10))
-	fmt.Println(algorithms.FastPowerIter(2, 10))
-
-	fmt.Println(algorithms.IsPrimeNumber(29))
-
-	fmt.Println(algorithms.FibRec(7))
-
-	fmt.Println(algorithms.FibIter(12))
-
-	fmt.Println(algorithms.ReverseString("i am programmist"))
-	fmt.Println(algorithms.ReverseString2("i am programmist"))
-
-	fmt.Println(algorithms.SquareRoot(56))
-	fmt.Println(algorithms.BSquareRoot(56))
+	//fmt.Println(algorithms.FactorialRec(10))
+	//fmt.Println(algorithms.FactorialIter(10))
+	//
+	//fmt.Println(algorithms.FastPowerRec(2, 10))
+	//fmt.Println(algorithms.FastPowerIter(2, 10))
+	//
+	//fmt.Println(algorithms.IsPrimeNumber(29))
+	//
+	//fmt.Println(algorithms.FibRec(7))
+	//
+	//fmt.Println(algorithms.FibIter(12))
+	//
+	//fmt.Println(algorithms.ReverseString("i am programmist"))
+	//fmt.Println(algorithms.ReverseString2("i am programmist"))
+	//
+	//fmt.Println(algorithms.SquareRoot(56))
+	//fmt.Println(algorithms.BSquareRoot(56))
 
 	algorithms.MinionGame("SUkhoverkhov")
 }
