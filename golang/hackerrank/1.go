cdc9623cf0e207e5fe2aa384a59bc9bd4119e0e4
Denis <denis.sukhoverkhov@yandex.ru>
2019-09-26 21:24:00 +0300

    HR: PROBLEM SOLVING 1

---
+++ golang/hackerrank/1.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func solveMeFirst(a, b uint32) uint32 {
+	return a + b
+}
+
+func main() {
+	var a, b, res uint32
+	fmt.Scanf("%v\n%v", &a, &b)
+	res = solveMeFirst(a, b)
+	fmt.Println(res)
+}
