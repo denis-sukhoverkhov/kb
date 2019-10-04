4223ba3eaae03b87ac34a60b5fc5f9a4614e38ec
Denis <denis.sukhoverkhov@yandex.ru>
2019-10-04 16:13:11 +0300

    HR - reverse array

---
+++ golang/hackerrank/reverse_array.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func reverseArray(a []int32) []int32 {
+	var new_arr []int32
+	for i := len(a) - 1; i >= 0; i-- {
+		new_arr = append(new_arr, a[i])
+	}
+	return new_arr
+}
+
+func main() {
+	arr := []int32{1, 4, 3, 2}
+	fmt.Println(reverseArray(arr))
+}
