39bb0d94389459cf59233fa8c0e4b1473cb16bdd
Denis <denis.sukhoverkhov@yandex.ru>
2019-10-06 20:35:01 +0300

    Python - work with linked lists

---
+++ python/hackerrank/insert_at_head.py
@@ -0,0 +1,41 @@
+
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+
+def insertNodeAtHead(llist, data):
+    if llist is None:
+        return SinglyLinkedListNode(node_data=data)
+
+    node = SinglyLinkedListNode(node_data=data)
+    node.next = llist
+
+    return node
+
+
+if __name__ == '__main__':
+
+    llist = SinglyLinkedList()
+
+    arr = [1, 2, 3, 4, 5]
+    for i in arr:
+        llist_head = insertNodeAtHead(llist.head, i)
+        llist.head = llist_head
---
+++ python/hackerrank/insert_at_tail.py
@@ -0,0 +1,45 @@
+
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+
+def insertNodeAtTail(head, data):
+
+    if head is None:
+        return SinglyLinkedListNode(node_data=data)
+
+    node = head
+    while node.next is not None:
+        node = node.next
+
+    node.next = SinglyLinkedListNode(node_data=data)
+
+    return head
+
+
+if __name__ == '__main__':
+
+    llist = SinglyLinkedList()
+
+    arr = [1, 2, 3, 4, 5]
+    for i in arr:
+        llist_head = insertNodeAtTail(llist.head, i)
+        llist.head = llist_head
