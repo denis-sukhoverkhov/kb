30ae044aa31ac3f34c5e8dfcc1fe05d5186e3cd1
Denis <denis.sukhoverkhov@yandex.ru>
2019-10-23 09:46:15 +0300

    python calls_counter_decorator

---
+++ python/sandbox/calls_counter_decorator.py
@@ -0,0 +1,26 @@
+
+def call_counter(call_limit):
+    def decorator(func):
+        def helper(calls={}):
+            key = func.__name__
+            calls[key] = 1 if key not in calls else calls[key] + 1
+            if calls[key] > call_limit:
+                raise Exception('exceeding the limit calls')
+
+        def wrapper(*args, **kwargs):
+            helper()
+            return func(*args, **kwargs)
+        return wrapper
+
+    return decorator
+
+
+@call_counter(3)
+def my_fun(t, c):
+    print(1)
+
+
+my_fun(4, c=5)
+my_fun(4, c=5)
+my_fun(4, c=5)
+my_fun(4, c=5)
