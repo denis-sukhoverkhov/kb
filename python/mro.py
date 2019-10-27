b25de2e5f00bb7e747f2a0bd0633d356a1970483
Denis <denis.sukhoverkhov@yandex.ru>
2019-10-27 14:26:47 +0300

    python MRO

---
+++ python/mro.py
@@ -0,0 +1,22 @@
+class A:
+    def process(self):
+        print('A process()')
+
+
+class B:
+    def process(self):
+        print('B process()')
+
+
+class C(A, B):
+    pass
+
+
+
+class D(C,B):
+    pass
+
+
+obj = D()
+obj.process()
+print(C.mro())   # print MRO for class C
