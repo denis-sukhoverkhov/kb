97f4a478c6c72185383a076189f974a282868f0e
Denis <denis.sukhoverkhov@yandex.ru>
2020-02-05 21:27:28 +0300

    yandex c

---
+++ python/yandex/c.py
@@ -0,0 +1,40 @@
+
+def gen(cnt, ):
+    m = (['('] * cnt) + ([')'] * cnt)
+
+    print(*m, sep="")
+
+    while True:
+        idx = len(m) - 1
+        cnt = 0
+
+        while idx >= 0:
+            if m[idx] == ')':
+                cnt -= 1
+            elif m[idx] == '(':
+                cnt += 1
+
+            if cnt < 0 and m[idx] == '(':
+                break
+            idx -= 1
+
+        if idx < 0:
+            break
+
+        m[idx] = ')'
+
+        v = (len(m) - idx + cnt) / 2 + idx
+        for i in range(idx + 1, len(m)):
+            if i <= v:
+                m[i] = '('
+            else:
+                m[i] = ')'
+        print(*m, sep="")
+
+
+if __name__ == '__main__':
+
+    import sys
+    n = int(sys.stdin.readline().strip())
+    gen(n)
+
