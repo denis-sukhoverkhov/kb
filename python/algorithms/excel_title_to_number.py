4b1a15b389ea29cb2471a0680c85e281bad55d73
Denis <denis.sukhoverkhov@yandex.ru>
2020-02-29 01:29:50 +0300

    convert_to_title: https://www.interviewbit.com/problems/excel-column-title/

--- python/algorithms/excel_title_to_number.py
+++ python/algorithms/excel_title_to_number.py
@@ -9,12 +9,32 @@ def title_to_number(A):
     return result
 
 
+def convert_to_title(A: int):
+
+    res = []
+    while A != 0:
+        ost = A % 26
+        if ost == 0:
+            res.append('Z')
+            A = A // 26 - 1
+        else:
+            res.append(chr(ord('A') + ost - 1))
+            A = A // 26
+
+    return "".join(res[::-1])
+
+
 if __name__ == '__main__':
-    print(title_to_number('A'))
-    print(title_to_number('AA'))
-    print(title_to_number('AB'))
-    print(title_to_number('ABC'))
-    print(title_to_number('AAA'))
-    print(title_to_number('AAAB'))
-    print(title_to_number('AZA'))
-    print(title_to_number('CDA'))
+    print('A=> ', title_to_number('A'))
+    print('AA=> ', title_to_number('AA'))
+    print('AB=> ', title_to_number('AB'))
+    print('ABC=> ', title_to_number('ABC'))
+    print('AAA=> ', title_to_number('AAA'))
+    print('AAAB=> ', title_to_number('AAAB'))
+    print('AZA=> ', title_to_number('AZA'))
+    print('CDA=> ', title_to_number('CDA'), "\n\n")
+    print('28=> ', convert_to_title(28))
+    print('18280=> ', convert_to_title(18280))
+    print('25=> ', convert_to_title(25))
+    print('26=> ', convert_to_title(26))
+    print('943566=> ', convert_to_title(943566))
