743bf46f5bc38319f3463ece81efdbdf4b7673db
Denis <denis.sukhoverkhov@yandex.ru>
2020-03-07 08:44:15 +0300

    cover_points

---
+++ python/algorithms/city_tour.py
@@ -0,0 +1,25 @@
+from itertools import permutations
+from typing import List
+
+
+def solve(A: int, B: List[int]):
+    all_cities = set([i for i in range(1, A+1)])
+    visited = [False] * len(all_cities)
+    for i in B:
+        visited[i] = True
+
+    res = 1
+    for n in range(1, len(not_visited) + 1):
+        res *= n
+
+    return res % (10**9+7)
+
+
+if __name__ == "__main__":
+    # a = 5
+    # b = [2, 5]
+    # print(solve(a, b))
+
+    a = 3
+    b = [1]
+    print(solve(a, b))
---
+++ python/algorithms/max_sum_contiguous_subarray.py
@@ -0,0 +1,21 @@
+def max_sum_contiguous_subarray(A):
+    # https://www.interviewbit.com/problems/max-sum-contiguous-subarray/
+
+    cur_max = A[0]
+    max_so_far = A[0]
+    for i in range(1, len(A)):
+        cur_max = max(A[i], cur_max + A[i])
+        max_so_far = max(cur_max, max_so_far)
+
+    return max_so_far
+
+
+if __name__ == "__main__":
+    print(max_sum_contiguous_subarray([-2, -3, 4, -1, -2, 1, 5, -3]))
+    print(max_sum_contiguous_subarray([-2, -3, -3]))
+
+    A = [97, 0, -461, -125, -404, -59, -322, -495, -288, -341, -449, -313, -192, -214, -389, -202, -183, -72, -416,
+         -455, -187, -242, -416, 39, -198, -338, -465, -248, -25, -398, -97, -461, -214, -423, -407, -77, -190, -67,
+         -178, -410, -160, 72, -350, -31, -85, -247, -319, -462, -303, -48, -354, -110, 17, 60, 19, 80, -218, -28,
+         -426, -366, -140, 50]
+    print(max_sum_contiguous_subarray(A))
