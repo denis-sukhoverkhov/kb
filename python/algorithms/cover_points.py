1461f7ab9a1d6fe43b61e7733e3d17111455cdcb
Denis <denis.sukhoverkhov@yandex.ru>
2020-03-10 20:01:18 +0300

    cover_points

---
+++ python/algorithms/cover_points.py
@@ -0,0 +1,19 @@
+
+def cover_points(A, B):
+
+    resp = 0
+    for i in range(len(A) - 1):
+        x, y = A[i], B[i]
+
+        next_x = A[i+1]
+        next_y = B[i + 1]
+
+        resp += max(abs(x-next_x), abs(y-next_y))
+
+    return resp
+
+
+if __name__ == "__main__":
+    A = [0, 0, 1]
+    B = [0, 1, 2]
+    print(cover_points(A, B))
