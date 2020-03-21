b4a2d3cdc94bc2d30d9b18b89732848de16c309f
Denis <denis.sukhoverkhov@yandex.ru>
2020-03-21 14:33:39 +0300

    ашкые repeated_number in array

---
+++ python/algorithms/repeated_number.py
@@ -0,0 +1,17 @@
+
+
+def repeated_number(A):
+    m = dict()
+
+    for i in A:
+        if i in m:
+            return i
+        else:
+            m[i] = None
+
+    return -1
+
+
+if __name__ == '__main__':
+    A = [3, 4, 1, 4, 1]
+    print(repeated_number(A))
