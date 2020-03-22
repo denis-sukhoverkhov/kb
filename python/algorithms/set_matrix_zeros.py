ee21c38c7d299bf9339e3f0fc7446429c069801d
Denis <denis.sukhoverkhov@yandex.ru>
2020-03-22 09:24:34 +0300

    set_matrix_zero

---
+++ python/algorithms/set_matrix_zeros.py
@@ -0,0 +1,31 @@
+def set_zeroes(A):
+    map_row = dict()
+    map_col = dict()
+
+    for i in range(len(A)):
+        for j in range(len(A[0])):
+            if A[i][j] == 0:
+                map_row[i] = None
+                map_col[j] = None
+
+    for r in map_row.keys():
+        A[r] = [0] * len(A[0])
+
+    for c in map_col.keys():
+        for r in A:
+            r[c] = 0
+
+    return A
+
+
+if __name__ == "__main__":
+    A = [[1, 0, 1],
+         [1, 1, 1],
+         [1, 1, 1]]
+    print(set_zeroes(A))
+
+    A = [
+        [0, 0],
+        [1, 1]
+    ]
+    print(set_zeroes(A))
