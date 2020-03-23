f823b55dd86ea9afb80b658cec711f69bcf59654
Denis <denis.sukhoverkhov@yandex.ru>
2020-03-23 08:08:46 +0300

    amazing_subarrays

---
+++ python/algorithms/amazing_subarrays.py
@@ -0,0 +1,18 @@
+# https://www.interviewbit.com/problems/amazing-subarrays/
+
+def amazing_subarrays(A):
+    vowel = ('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U')
+
+    ct_amazing_strings = 0
+
+    ln = len(A)
+    for k, v in enumerate(A):
+        if v in vowel:
+            ct_amazing_strings += ln - k
+
+    return ct_amazing_strings % 10003
+
+
+if __name__ == "__main__":
+    st = "ABEC"
+    print(amazing_subarrays(st))
