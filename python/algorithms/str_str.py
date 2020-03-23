b1d92c743b876dfab205272cdf87af6cc8c82d7d
Denis <denis.sukhoverkhov@yandex.ru>
2020-03-23 08:09:20 +0300

    str_str: https://www.interviewbit.com/problems/implement-strstr/

---
+++ python/algorithms/str_str.py
@@ -0,0 +1,22 @@
+# https://www.interviewbit.com/problems/implement-strstr/
+
+
+def str_str(A, B):
+    res_idx = -1
+
+    if not A or not B or len(B) > len(A):
+        return res_idx
+
+    ln_b = len(B)
+
+    for i in range(len(A)):
+        if A[i:i+ln_b] == B:
+            return i
+
+    return res_idx
+
+
+if __name__ == "__main__":
+    s = 'HelloWorld'
+    sb = 'World'
+    print(str_str(s, sb))
