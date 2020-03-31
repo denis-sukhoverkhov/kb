3a563be973801b2a855aa4a9e19bf5f68f2de131
Denis <denis.sukhoverkhov@yandex.ru>
2020-03-31 15:51:34 +0300

    # https://www.interviewbit.com/problems/length-of-last-word/
    length_of_last_word(A):

---
+++ python/algorithms/length_of_last_word.py
@@ -0,0 +1,35 @@
+# https://www.interviewbit.com/problems/length-of-last-word/
+
+def length_of_last_word(A):
+
+    start_pos = 0
+    end_word = False
+    end_pos = 0
+    for i in range(len(A)):
+        if A[i] == ' ':
+            start_word = False
+            end_word = True
+            end_pos = i
+        else:
+            start_word = True
+
+        if start_word and end_word:
+            start_pos = i
+            end_word = False
+
+    end_pos = len(A) if end_pos <= start_pos else end_pos
+    return len(A[start_pos:end_pos].strip())
+
+
+if __name__ == "__main__":
+    s = "Hello World"
+    print(length_of_last_word(s))
+
+    s = ""
+    print(length_of_last_word(s))
+
+    s = "Hello World "
+    print(length_of_last_word(s))
+
+    s = "Hello World  "
+    print(length_of_last_word(s))
