06d66824312f31e65c19a341e9003de869da8aec
Denis <denis.sukhoverkhov@yandex.ru>
2020-04-01 20:32:42 +0300

    max_abs_diff

---
+++ python/algorithms/max_abs_diff.py
@@ -0,0 +1,24 @@
+
+
+def max_arr(A):
+
+    max1 = -90000000000000000000
+    min1 = 900000000000000000000
+    max2 = max1
+    min2 = min1
+
+    for i in range(len(A)):
+
+        max1 = max(max1, A[i] + i)
+        min1 = min(min1, A[i] + i)
+        max2 = max(max2, A[i] - i)
+        min2 = min(min2, A[i] - i)
+
+    return max(max1 - min1, max2 - min2)
+
+
+if __name__ == "__main__":
+    array = [-70, -64, -6, -56, 64,
+             61, -57, 16, 48, -98]
+
+    print(max_arr(array))
