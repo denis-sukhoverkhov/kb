4a690df85b039ffe949432dee03977233535cac5
Denis <denis.sukhoverkhov@yandex.ru>
2020-04-02 10:35:06 +0300

    reverse_the_string: https://www.interviewbit.com/problems/reverse-the-string/

---
+++ python/algorithms/reverse_the_string.py
@@ -0,0 +1,17 @@
+# https://www.interviewbit.com/problems/reverse-the-string/
+
+def reverse_the_string(A: str):
+
+    list_of_words = A.split(' ')
+
+    filtered_words = [i for i in list_of_words if i]
+
+    return ' '.join(filtered_words[::-1])
+
+
+if __name__ == "__main__":
+    A = "the sky is blue"
+    print(reverse_the_string(A))
+
+    A = "this is ib"
+    print(reverse_the_string(A))
