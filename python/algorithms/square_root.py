331fbbd86a9adf34ea536ac31d8009e4911cde59
Denis <denis.sukhoverkhov@yandex.ru>
2020-04-23 22:37:40 +0300

    Square root of an integer # https://www.interviewbit.com/problems/square-root-of-integer/

--- python/algorithms/square_root.py
+++ python/algorithms/square_root.py
@@ -1,4 +1,7 @@
 # квадратный корень - перебор
+import math
+
+
 def square_root(number):
     num_iterations = 0
     epicilon = 0.0001
@@ -31,6 +34,40 @@ def bsquare_root(number):
     return 'root = {}, iteration = {}'.format(guess, num_iterations)
 
 
+def root(A):
+    # https://www.interviewbit.com/problems/square-root-of-integer/
+    # Square root of an integer
+    if A in (0, 1):
+        return A
+
+    start = 0
+    end = A
+
+    ans = None
+    while start <= end:
+        mid = (start + end) // 2
+
+        if mid*mid == A:
+            return mid
+
+        if mid * mid > A:
+            end = mid - 1
+        else:
+            start = mid + 1
+            ans = mid
+
+    return ans
+
+
+
 if __name__ == "__main__":
-    print(square_root(123.45655))
-    print(bsquare_root(123.45655))
+    # print(square_root(123.45655))
+    # print(bsquare_root(123.45655))
+    assert root(16) == 4
+    assert root(2) == 1
+    assert root(0) == 0
+    assert root(1) == 1
+    assert root(3) == 1
+    assert root(5) == 2
+    assert root(6) == 2
+    assert root(169078009) == 13003
