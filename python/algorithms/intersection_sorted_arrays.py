fc737448a02f7b2b4cbb01808a9285b35effcd88
Denis <denis.sukhoverkhov@yandex.ru>
2020-04-26 02:28:19 +0300

    intersect sorted arrays https://www.interviewbit.com/problems/intersection-of-sorted-arrays/

---
+++ python/algorithms/intersection_sorted_arrays.py
@@ -0,0 +1,27 @@
+
+def intersect(A, B):
+
+    i = j = 0
+
+    res = []
+    while i < len(A) and j < len(B):
+        if A[i] < B[j]:
+            i += 1
+        elif B[j] < A[i]:
+            j += 1
+        else:
+            res.append(A[i])
+            i += 1
+            j += 1
+
+    return res
+
+
+if __name__ == "__main__":
+    A = [1, 2, 3, 3, 4, 5, 6]
+    B = [3, 3, 5]
+    assert intersect(A, B) == [3, 3, 5]
+
+    A = [1, 2, 3, 3, 4, 5, 6]
+    B = [3, 5]
+    assert intersect(A, B) == [3, 5]
