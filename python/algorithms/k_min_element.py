692e89b334b91ee817f1094046a1fb1ae3caf32f
Denis <denis.sukhoverkhov@yandex.ru>
2020-04-27 00:50:36 +0300

    1) numrange 2) k_min_element

---
+++ python/algorithms/k_min_element.py
@@ -0,0 +1,12 @@
+
+def k_min_el(A, B):
+
+    a = sorted(A)
+
+    return a[B-1]
+
+
+if __name__ == "__main__":
+    A = [2, 1, 4, 3, 2]
+    B = 3
+    assert k_min_el(A, B) == 2
---
+++ python/algorithms/numrange.py
@@ -0,0 +1,29 @@
+import copy
+
+
+def numrange(A, B, C):
+    tmp_sum = 0
+
+    ranges = []
+    for i in range(len(A)):
+        tmp_sum += 0
+        j = i
+        tmp_r = []
+        while tmp_sum <= C and A[i]<C  and j < len(A):
+            tmp_sum += A[j]
+            tmp_r.append(A[j])
+            j += 1
+
+            if B <= tmp_sum <= C:
+                ranges.append(copy.copy(tmp_r))
+
+        tmp_sum = 0
+
+    return len(ranges)
+
+
+if __name__ == "__main__":
+    A = [10, 5, 1, 0, 2]
+    B = 6
+    C = 8
+    assert numrange(A, B, C) == 3
