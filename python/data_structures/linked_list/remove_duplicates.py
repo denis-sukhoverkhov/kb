955743dc381e794be1ddd16351a54ecee224c43a
Denis <denis.sukhoverkhov@yandex.ru>
2020-05-04 23:12:20 +0300

    is palindrome linked list? https://www.interviewbit.com/problems/palindrome-list/

--- python/data_structures/linked_list/remove_duplicates.py
+++ python/data_structures/linked_list/remove_duplicates.py
@@ -77,13 +77,69 @@ class Solution:
 
         return curr_head
 
+    def lPalin(self, A):
+        if not A or A.next is None:
+            return 1
+
+        p, q = A, A
+        start_first = p
+        start_second = q
+        while True:
+            p = p.next.next
+            if p is None:
+                start_second = q.next
+                break
+            if p.next is None:
+                start_second = q.next.next
+                break
+            q = q.next
+
+        q.next = None
+
+        # reverse linked list
+        curr = start_second
+        prev = None
+        while curr:
+            next = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next
+
+        start_second = prev
+
+        head1 = start_first
+        head2 = start_second
+        while head1 and head2:
+            if head1.data != head2.data:
+                return 0
+            head1 = head1.next
+            head2 = head2.next
+        return 1
+
+
 
 if __name__ == "__main__":
     mylist = Solution()
     head = None
-    lst = [1, 1, 2, 2, 3, 4, 4, 4, 5, 5, 6, 7, 7, 7, 9]
+    # lst = [1, 1, 2, 2, 3, 4, 4, 4, 5, 5, 6, 7, 7, 7, 9]
+    # for i in range(len(lst)):
+    #     head = mylist.insert(head, lst[i])
+    # # head = mylist.removeDuplicates(head)
+    # head = mylist.remove_all_duplicates(head)
+    # mylist.display(head)
+
+    mylist = Solution()
+    head = None
+    lst = ['a', 'b', 'c', 'b', 'a']
     for i in range(len(lst)):
         head = mylist.insert(head, lst[i])
     # head = mylist.removeDuplicates(head)
-    head = mylist.remove_all_duplicates(head)
-    mylist.display(head)
+    assert mylist.lPalin(head) == 1
+
+    mylist = Solution()
+    head = None
+    lst = ['a', 'b', 'b', 'a']
+    for i in range(len(lst)):
+        head = mylist.insert(head, lst[i])
+    # head = mylist.removeDuplicates(head)
+    assert mylist.lPalin(head) == 1
