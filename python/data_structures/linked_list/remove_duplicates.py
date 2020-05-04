204c0b8f4d685d7dc913c3f88e12c3bc4b22888f
Denis <denis.sukhoverkhov@yandex.ru>
2020-05-04 22:48:24 +0300

    remove_all_duplicates in linkedlist

--- python/data_structures/linked_list/remove_duplicates.py
+++ python/data_structures/linked_list/remove_duplicates.py
@@ -39,12 +39,51 @@ class Solution:
 
         return main_head
 
+    def remove_all_duplicates(self, A):
+        curr_head = None
+
+        while A and curr_head is None:
+            cur_val = A.data
+
+            f = False
+            while A.next and cur_val == A.next.data:
+                A = A.next
+                f = True
+            else:
+                if f:
+                    A = A.next
+                else:
+                    curr_head = A
+                    break
+
+        if curr_head:
+            A = curr_head.next
+            last_node = curr_head
+            while A:
+                cur_val = A.data
+
+                f = False
+                while A.next and cur_val == A.next.data:
+                    A = A.next
+                    f = True
+                else:
+                    if not f:
+                        last_node.next = A
+                        last_node = last_node.next
+                    else:
+                        last_node.next = None
+                A = A.next
+                # last_node.next.next = None
+
+        return curr_head
+
 
 if __name__ == "__main__":
     mylist = Solution()
     head = None
-    lst = [1, 1, 2, 3]
-    for i in range(4):
+    lst = [1, 1, 2, 2, 3, 4, 4, 4, 5, 5, 6, 7, 7, 7, 9]
+    for i in range(len(lst)):
         head = mylist.insert(head, lst[i])
-    head = mylist.removeDuplicates(head)
+    # head = mylist.removeDuplicates(head)
+    head = mylist.remove_all_duplicates(head)
     mylist.display(head)
