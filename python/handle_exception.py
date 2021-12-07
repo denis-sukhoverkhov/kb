fdb6870c12ada8d7d749104070afa3a5d59ba33f
Denis Sukhoverkhov <denis.sukhoverkhov@yandex.ru>
2021-12-07 19:32:56 +0300

    leetcode search_insert_position

---
+++ python/handle_exception.py
@@ -0,0 +1,27 @@
+import sys
+import asyncio
+
+
+def handle_exception(exc_type, exc_value, exc_traceback):
+    print(f'2: {exc_value!r}')
+
+
+sys.excepthook = handle_exception
+from asyncio import sleep
+
+
+async def f():
+    await sleep(1.1)
+    1 / 0
+
+
+async def main():
+    try:
+        asyncio.create_task(f())
+        await asyncio.sleep(0.1)
+    except Exception as e:
+        print(f'2: {e!r}')
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
