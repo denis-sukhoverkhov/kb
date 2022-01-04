eaec24d75660d62787ccbb8294a0aa5382b4a0f1
Denis Sukhoverkhov <denis.sukhoverkhov@yandex.ru>
2022-01-04 21:27:59 +0300

    leetcode: add_two_numbers

---
+++ python/leetcode/add_two_numbers.py
@@ -0,0 +1,66 @@
+from typing import Optional
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+    def to_array(self):
+        arr = []
+        head = self
+        while head is not None:
+            arr.append(head.val)
+            head = head.next
+
+        return arr
+
+
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+
+        n1 = l1
+        n2 = l2
+
+        head = None
+        current_node = None
+        offset = 0
+        while n1 is not None or n2 is not None:
+
+            val1 = n1.val if n1 is not None else 0
+            val2 = n2.val if n2 is not None else 0
+            summ = val1 + val2 + offset
+            offset = 0
+            if summ > 9:
+                offset = 1
+
+            val = summ % 10
+            if head is None:
+                head = ListNode(val)
+                current_node = head
+            else:
+                current_node.next = ListNode(val)
+                current_node = current_node.next
+
+            n1 = n1.next if n1 else None
+            n2 = n2.next if n2 else None
+        else:
+            if offset != 0:
+                current_node.next = ListNode(offset)
+        return head
+
+
+if __name__ == "__main__":
+    obj = Solution()
+
+    number1 = ListNode(9,
+                       ListNode(9, ListNode(9, ListNode(9, ListNode(9, ListNode(9, ListNode(9)))))))
+    number2 = ListNode(9, ListNode(9, ListNode(9, ListNode(9))))
+    assert obj.addTwoNumbers(number1, number2).to_array() == [8, 9, 9, 9, 0, 0, 0, 1]
+
+    number1 = ListNode(2, ListNode(4, ListNode(3)))
+    number2 = ListNode(5, ListNode(6, ListNode(4)))
+    assert obj.addTwoNumbers(number1, number2).to_array() == [7, 0, 8]
+
+    assert obj.addTwoNumbers(ListNode(0), ListNode(0)).val == ListNode(0).val
--- theory/links.md
+++ theory/links.md
@@ -9,3 +9,7 @@
 [Iterators](https://www.programiz.com/python-programming/iterator)
 
 [c100k problem](https://qunfei.blog/2016/09/20/from-c10k-to-c100k-problem-push-over-1000000-messages-to-web-clients-on-1-machine-simultaneously/)
+
+[Про Kafka (основы)](https://www.youtube.com/watch?v=-AZOi3kP9Js&ab_channel=%D0%92%D0%BB%D0%B0%D0%B4%D0%B8%D0%BC%D0%B8%D1%80%D0%91%D0%BE%D0%B3%D0%B4%D0%B0%D0%BD%D0%BE%D0%B2%D1%81%D0%BA%D0%B8%D0%B9)
+
+[MurmurHash2](https://wiki2.net/MurmurHash2)
