533e2e2d348f3127701768bf283d36c3a051aa3f
Denis Sukhoverkhov <denis.sukhoverkhov@yandex.ru>
2022-01-09 16:08:05 +0300

    leetcode: reverse_string

---
+++ python/leetcode/reverse_string.py
@@ -0,0 +1,26 @@
+from typing import List
+
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        left, right = 0, len(s) - 1
+
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
+
+
+if __name__ == "__main__":
+    obj = Solution()
+
+    s = ["h", "e", "l", "l", "o"]
+    obj.reverseString(s)
+    assert s == ["o", "l", "l", "e", "h"]
+
+    s = ["H", "a", "n", "n", "a", "h"]
+    obj.reverseString(s)
+    assert s == ["h","a","n","n","a","H"]
