919bcf67d0deb0c5178452f527f828230ef672cc
Denis Sukhoverkhov <denis.sukhoverkhov@yandex.ru>
2022-03-31 10:35:01 +0300

    leetcode: validate_binary_search_tree

---
+++ python/leetcode/validate_binary_search_tree.py
@@ -0,0 +1,41 @@
+# Definition for a binary tree node.
+from typing import Optional
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+
+        def traceback(node, left, right):
+            if node is None:
+                return True
+
+            if node.val <= left or node.val >= right:
+                return False
+
+            return traceback(node.left, left=left, right=node.val) \
+                   and traceback(node.right, left=node.val, right=right)
+
+        return traceback(root, float('-inf'), float('+inf'))
+
+
+if __name__ == "__main__":
+    obj = Solution()
+
+    head = TreeNode(2, left=TreeNode(1), right=TreeNode(3))
+    assert obj.isValidBST(head) is True
+
+    head = TreeNode(5, left=TreeNode(4), right=TreeNode(6, left=TreeNode(3), right=TreeNode(7)))
+    assert obj.isValidBST(head) is False
+
+    head = TreeNode(2, left=TreeNode(2), right=TreeNode(2))
+    assert obj.isValidBST(head) is False
+
+    head = TreeNode(5, left=TreeNode(1), right=TreeNode(4, left=TreeNode(3), right=TreeNode(6)))
+    assert obj.isValidBST(head) is False
+
