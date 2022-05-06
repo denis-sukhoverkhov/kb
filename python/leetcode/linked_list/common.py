f763244b7a8c3b317c1b9c6eaea3b37c92164ca7
Denis Sukhoverkhov <denis.sukhoverkhov@yandex.ru>
2022-05-06 13:20:00 +0300

    leetcode: ListNode

---
+++ python/leetcode/linked_list/common.py
@@ -0,0 +1,26 @@
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+    @classmethod
+    def to_list(cls, array):
+        head = ListNode()
+
+        current_node = head
+        for i in array:
+            current_node.next = ListNode(i)
+            current_node = current_node.next
+
+        return head.next
+
+    def to_array(self):
+
+        curr = self
+        result = []
+        while curr:
+            result.append(curr.val)
+            curr = curr.next
+
+        return result
