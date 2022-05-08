da5eacf9bcab4bea1eee9a209fc21d7551b38aed
Denis Sukhoverkhov <denis.sukhoverkhov@yandex.ru>
2022-05-08 05:22:09 +0300

    leetcode: reverse_linked_list

---
+++ python/leetcode/flatten_nested_list_iterator.py
@@ -0,0 +1,16 @@
+from python.leetcode.libs.nested_integer import NestedInteger
+
+
+class NestedIterator:
+    def __init__(self, nestedList: [NestedInteger]):
+        pass
+
+    def next(self) -> int:
+        pass
+
+    def hasNext(self) -> bool:
+        pass
+
+# Your NestedIterator object will be instantiated and called as such:
+# i, v = NestedIterator(nestedList), []
+# while i.hasNext(): v.append(i.next())
---
+++ python/leetcode/linked_list/reverse_linked_list.py
@@ -0,0 +1,35 @@
+from typing import Optional
+from python.leetcode.linked_list.common import ListNode
+
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+
+        root = head
+        current_head = ListNode()
+        current_head.next = root
+        while root and root.next:
+            next_node = root.next
+            root.next = next_node.next
+            next_node.next = current_head.next
+            current_head.next = next_node
+
+        return current_head.next
+
+
+if __name__ == "__main__":
+    obj = Solution()
+
+    head = [1, 2, 3, 4, 5]
+    head = ListNode.to_list(head)
+    res = obj.reverseList(head)
+    assert res.to_array() == [5, 4, 3, 2, 1]
+
+    head = [1, 2]
+    head = ListNode.to_list(head)
+    res = obj.reverseList(head)
+    assert res.to_array() == [2, 1]
+
+    head = []
+    head = ListNode.to_list(head)
+    assert obj.reverseList(head) is None
