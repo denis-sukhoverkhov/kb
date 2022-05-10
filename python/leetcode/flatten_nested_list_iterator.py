5ea41ff3c0ef8662522339a63912e9007278e16d
Denis Sukhoverkhov <denis.sukhoverkhov@yandex.ru>
2022-05-10 09:45:22 +0300

    leetcode: flatten_nested_list_iterator

--- python/leetcode/flatten_nested_list_iterator.py
+++ python/leetcode/flatten_nested_list_iterator.py
@@ -3,14 +3,37 @@ from python.leetcode.libs.nested_integer import NestedInteger
 
 class NestedIterator:
     def __init__(self, nestedList: [NestedInteger]):
-        pass
+        self.lst = []
+        self.pointer = 0
+
+        def traversal(lst):
+            if lst.isInteger():
+                self.lst.append(lst)
+                return
+
+            for k in lst.getList():
+                traversal(k)
+
+        for i in nestedList:
+            traversal(i)
 
     def next(self) -> int:
-        pass
+        res = self.lst[self.pointer]
+        self.pointer += 1
+        return res.getInteger()
 
     def hasNext(self) -> bool:
-        pass
+        return len(self.lst) > self.pointer
 
-# Your NestedIterator object will be instantiated and called as such:
-# i, v = NestedIterator(nestedList), []
-# while i.hasNext(): v.append(i.next())
+
+if __name__ == "__main__":
+    nestedList = [[1, 1], 2, [1, 1]]
+    nestedList = NestedInteger.to_nested(nestedList)
+    obj = NestedIterator(nestedList.val)
+    assert obj.next() == 1
+    assert obj.next() == 1
+    assert obj.next() == 2
+    assert obj.hasNext() is True
+    assert obj.next() == 1
+    assert obj.next() == 1
+    assert obj.hasNext() is False
