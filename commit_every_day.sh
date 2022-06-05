e18c83b5660524fd75a33697c4ebe0065d776874
Denis Sukhoverkhov <denis.sukhoverkhov@yandex.ru>
2022-06-05 13:06:37 +0300

    commit_every_day

---
+++ commit_every_day.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd ~/projects/kb/ || exit
+git add .
+FILE_NAME_WITH_PATH="$(git status --porcelain | sed s/^...// | head -n 1)"
+FILE_NAME="$(basename "$FILE_NAME_WITH_PATH" | sed 's/\(.*\)\..*/\1/')"
+echo $FILE_NAME
+git commit -m "$FILE_NAME" $FILE_NAME_WITH_PATH
+git push -f
