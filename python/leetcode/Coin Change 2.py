bb95db263326cdf2c5868a4ace7d20e7292eadfd
Denis Sukhoverkhov <denis.sukhoverkhov@yandex.ru>
2022-09-26 21:30:00 +0500

    leetcode: Coin Change 2

---
+++ python/leetcode/Coin Change 2.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        pass
+
+
+if __name__ == "__main__":
+    obj = Solution()
+
+    amount = 5
+    coins = [1, 2, 5]
+    assert obj.change(amount, coins) == 4
